02bb0f4ec9fff5021f9d6fb121abbb9172a82977
Carlos Filipe <thecarlosfilipe@gmail.com>
2024-06-07 20:09:47 -0300

    Add Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,42 @@
-export function Post(props) {
+import styles from './Post.module.css';
+
+export function Post() {
   return (
-    <>
-      <p>{props.content}</p>
-      <small>{props.author}</small>
-    </>
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img className={styles.avatar} src="https://github.com/carlozfilipe.png" />
+          <div className={styles.authorInfo}>
+            <strong>Carlos Filipe</strong>
+            <span>Software Developer</span>
+          </div>
+        </div>
+
+        <time title="07 de Junho às 18:01h" dateTime="2024-06-07 18:01:00">Publicado há 1h</time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p><a href="">jane.design/doctorcare</a></p>
+        <p>
+          <a href="">#novoprojeto</a>{' '}
+          <a href="">#nlw</a>{' '}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder="Deixe um comentário"
+        />
+
+        <footer>
+          <button type="submit">Publicar</button>
+        </footer>
+      </form>
+    </article>
   )
 }

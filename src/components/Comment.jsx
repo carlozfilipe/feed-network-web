4488199266469b1a5704de53082b342cb5c2b57e
Carlos Filipe <thecarlosfilipe@gmail.com>
2024-06-07 21:45:05 -0300

    Add Comment component

---
+++ src/components/Comment.jsx
@@ -0,0 +1,35 @@
+import styles from './Comment.module.css';
+
+import { ThumbsUp, Trash } from '@phosphor-icons/react';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img src="https://github.com/gaearon.png" alt="" />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Dan Abramov</strong>
+              <time title="07 de Junho às 20:34h" dateTime="2024-06-07 20:34:18">Cerca de 1h atrás</time>
+            </div>
+
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>Very good, bro! 👏👏</p>
+        </div>
+
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,3 +1,4 @@
+import { Comment } from './Comment';
 import styles from './Post.module.css';
 
 export function Post() {
@@ -16,13 +17,13 @@ export function Post() {
       </header>
 
       <div className={styles.content}>
-        <p>Fala galeraa 👋</p>
-        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-        <p><a href="">jane.design/doctorcare</a></p>
+        <p>Fala, galerinha! 👋</p>
+        <p>Acabei de subir mais um projeto no meu portfólio. O nome do projeto é Feed Network 🚀</p>
+        <p><a href="https://feed-network.netlify.app/" target='_blank'>https://feed-network.netlify.app/</a></p>
         <p>
-          <a href="">#novoprojeto</a>{' '}
-          <a href="">#nlw</a>{' '}
-          <a href="">#rocketseat</a>
+          <a href="#">#newproject</a>{' '}
+          <a href="#">#reactjs</a>{' '}
+          <a href="#">#github</a>
         </p>
       </div>
 
@@ -37,6 +38,12 @@ export function Post() {
           <button type="submit">Publicar</button>
         </footer>
       </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
     </article>
   )
 }

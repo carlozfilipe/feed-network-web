2ca9b2336865de4b381f4f2a9cdc3bcd3d86dc29
Carlos Filipe <thecarlosfilipe@gmail.com>
2024-06-08 14:00:49 -0300

    Add responsiveness

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,3 +1,4 @@
+import { Avatar } from './Avatar';
 import { PencilLine } from '@phosphor-icons/react';
 
 import styles from './Sidebar.module.css';
@@ -11,10 +12,7 @@ export function Sidebar() {
       />
 
       <div className={styles.profile}>
-        <img
-          className={styles.avatar}
-          src="https://github.com/carlozfilipe.png"
-        />
+        <Avatar src='https://github.com/carlozfilipe.png' />
         <strong>Carlos Filipe</strong>
         <span>Software Developer</span>
       </div>
